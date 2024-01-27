7c17ff6e7a66662107b1a5b73ded73558a7058d7
roverine420 <gabriel.rover@hotmail.com>
2024-01-27 16:02:33 -0300

    CRUD simples com rotas, GET, POST, PUT e DELETE

---
+++ 05_projetoSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/ResourcesNotFaundException.java
@@ -0,0 +1,18 @@
+package br.com.erudio.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+
+
+public class ResourcesNotFaundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourcesNotFaundException(String ex) {
+		super(ex);
+	}
+
+	
+}
